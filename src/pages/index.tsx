30adcd1e4d6842914c251afa24a6714fc78fc8b6
João Cardoso <jvcq13@gmail.com>
2022-03-14 23:28:41 -0300

    added header + static home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 interface Post {
   uid?: string;
@@ -25,10 +29,36 @@ interface HomeProps {
 }
 
 export default function Home() {
-  // TODO
-  return(
-    <div>hello</div>
-  )
+  return (
+    <>
+      <Header />
+      <div className={styles.contentContainer}>
+        <Link href="/">
+          <a>
+            <h1>Como utilizar Hooks</h1>
+            <p>pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.postInfoContainer}>
+              <div>
+                <FiCalendar />
+                <time>15 Mar 2021</time>
+              </div>
+              <div>
+                <FiUser />
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </a>
+        </Link>
+        <Link href="/">
+          <a>
+            <h1>Como utilizar Hooks</h1>
+            <p>pensando em sincronização em vez de ciclos de vida.</p>
+            <time>15 Mar 2021</time> <span>Joseph Oliveira</span>
+          </a>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 // export const getStaticProps = async () => {
